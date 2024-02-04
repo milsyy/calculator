1ea5d17f096ba1b55e00ee2011cf31c6fc9d444a
milsyy <128106812+milsyy@users.noreply.github.com>
2024-02-04 17:01:24 -0500

    Updated JS to store and display values on click

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+display = document.querySelector(".first-layer");
+buttons = document.querySelector(".third-layer");
+clear = document.querySelector(".clear");
+
 let firstNum;
 let operator;
 let secondNum;
@@ -18,3 +22,43 @@ const operate = (firstNum, operator, secondNum) => {
     divide(firstNum, secondNum);
   }
 };
+
+const updateDisplay = (e) => {
+  if (!isNaN(e.target.textContent) && display.textContent === "0") {
+    display.textContent = e.target.textContent;
+    firstNum = display.textContent;
+  } else if (!isNaN(e.target.textContent) && operator == null) {
+    display.textContent += e.target.textContent;
+    firstNum += e.target.textContent;
+  } else if (
+    isNaN(e.target.textContent) &&
+    e.target.textContent != "." &&
+    e.target.textContent != "="
+  ) {
+    operator = e.target.textContent;
+  } else if (
+    !isNaN(e.target.textContent) &&
+    operator != null &&
+    secondNum == null
+  ) {
+    display.textContent = e.target.textContent;
+    secondNum = display.textContent;
+  } else if (
+    !isNaN(e.target.textContent) &&
+    operator != null &&
+    secondNum != null
+  ) {
+    display.textContent += e.target.textContent;
+    secondNum += e.target.textContent;
+  }
+};
+
+const clearDisplay = () => {
+  display.textContent = "0";
+  firstNum = null;
+  secondNum = null;
+  operator = null;
+};
+
+buttons.addEventListener("click", updateDisplay);
+clear.addEventListener("click", clearDisplay);
