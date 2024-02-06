32191c3ef08ee543a05d5a35795e8b33811565f5
milsyy <128106812+milsyy@users.noreply.github.com>
2024-02-06 00:35:50 -0500

    fixed decimal bug

--- script.js
+++ script.js
@@ -106,13 +106,22 @@ const clearDisplay = () => {
   firstNum = null;
   secondNum = null;
   operator = null;
+  decimal = false;
 };
 
 const deleteDisplay = () => {
   if (secondNum == null) {
+    if (firstNum.charAt(firstNum.length - 1) == ".") {
+      decimal = false;
+    }
+
     display.textContent = firstNum.slice(0, firstNum.length - 1);
     firstNum = firstNum.slice(0, firstNum.length - 1);
   } else if (secondNum != null) {
+    if (secondNum.charAt(secondNum.length - 1) == ".") {
+      decimal = false;
+    }
+
     display.textContent = secondNum.slice(0, secondNum.length - 1);
     secondNum = secondNum.slice(0, secondNum.length - 1);
   }
